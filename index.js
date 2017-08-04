02c511157e9141cbfce0f52b968f1d10bfa9a4c8
Fabricio C Zuardi <fabricio@fabricio.org>
2017-08-04 00:26:44 -0300

    remove the hardcoded user db add because we now have a way to do it with graphql

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const express = require("express");
-const { updateUser } = require("./lib/db");
 const config = require("./lib/config");
 const api = require("./lib/api");
 
+const { port, path } = config.api;
 const app = express();
-app.use(config.api.path, api);
-
-// add a users in the db if empty and start the api server
-// updateUser({ name: "foo2" }, () => null);
-updateUser(
-    {
-        id: "aa1425ef-c338-4be6-a144-2acb361a8e31",
-        name: "John Doe",
-        email: "mail@example.com"
-    },
-    (err, user) => {
-        console.log("test user update", { err }, { user });
-        if (err) {
-            throw err;
-        }
-        app.listen(config.api.port);
-        console.log("Running a GraphQL API server at localhost:4000/graphql");
-    }
-);
+app.use(path, api);
+app.listen(port);
+console.log(`Running a GraphQL API server at localhost:${port}${path}`);
